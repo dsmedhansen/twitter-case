b32be15a324ded85bebe17bbab07b230464af7fb
dsmedhansen <d.smed.hansen@gmail.com>
2018-09-25 21:05:25 +0200

    Added script to process electoral data

---
+++ electoral_data.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 20 15:16:32 2018
+
+@author: Daniel
+"""
+import os
+import pandas as pd
+
+#%%
+
+PATH = '/Users/Daniel/twitter-case/'
+os.chdir(PATH)
+print("Working directory: %s" % os.getcwd() )
+
+primary = pd.read_csv("primary_results.csv")
+demographics = pd.read_csv("county_facts.csv")
+
+# Get winners and vote-shares
+
+#%%
+
+votes = pd.DataFrame.groupby(['party' == 'Republican'], as_index = False)
+
+votes.info()
+votes.describe()
+
+list(votes) # Print object
+
