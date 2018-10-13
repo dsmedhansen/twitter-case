a5bf1df802b15fe934812cf15ecc40cbdca69ade
dsmedhansen <d.smed.hansen@gmail.com>
2018-10-13 09:01:12 +0200

    Added 5 different machine learning models and compared results

--- insta_analysis.py
+++ insta_analysis.py
@@ -92,23 +92,172 @@ df =  df.drop(
 
 del im_anp_obj_face_frame
 
+#%%
+# PERMA distribution by gender
+
+# Grade distribution by address : use code for later
+"""
+sns.kdeplot(df.ix[df['address'] == 'U', 'Grade'], label = 'Urban', shade = True)
+sns.kdeplot(df.ix[df['address'] == 'R', 'Grade'], label = 'Rural', shade = True)
+plt.xlabel('Grade'); plt.ylabel('Density'); plt.title('Density Plot of Final Grades by Location');
+"""
 
 #%%
 
 # Enrich data with one-hot vectors aka dummy-variables
 
 df_enriched = pd.get_dummies(df, columns=['image_filter', 'face_smile', 'face_gender', 'face_emo'])
-sample = df_enriched.sample(frac=0.01, replace=False) # 1% sample becasue my laptop sucks
-sample['PERMA'].nunique() # Same abount of unique PERMA-scores as in whole dataset
-#sample.to_csv("/Users/Daniel/Desktop/enriched_df.csv", sep=";" , index=False)
+#sample = df_enriched.sample(frac=0.01, replace=False) # 1% sample becasue my laptop sucks
+#sample['PERMA'].nunique() # Same abount of unique PERMA-scores as in whole dataset
+#df_enriched.to_csv("/Users/Daniel/Desktop/enriched_df.csv", sep=";" , index=False)
+
+#%%
+
+df_enriched = pd.read_csv("/Users/Daniel/Desktop/enriched_df.csv", sep=";")
+
+#%%
+
+# And now we do the feature selection
+
+df_enriched.corr()['PERMA'].sort_values()
 
 
-#%% And now for the fun stuff
-        # Release da tensorflow... 
+#%%
 
 
+# Standard ML Models for comparison
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import ElasticNet
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+
+# Splitting data into training/testing
+from sklearn.model_selection import train_test_split
+
+#%%
 
 
+# Takes in a dataframe, finds the most correlated variables with the
+# grade and returns training and testing datasets
+def format_data(df):
+    # Targets are perma scores
+    labels = df['PERMA']
+    
+    # Drop the school and the grades from features
+    #df = df.drop(columns=['school', 'G1', 'G2', 'percentile'])
+    
+    # One-Hot Encoding of Categorical Variables
+    #df = pd.get_dummies(df)
+    
+    # Find correlations with the Grade
+    most_correlated = df.corr().abs()['PERMA'].sort_values(ascending=False)
+    
+    # Maintain the top 6 most correlation features with Grade
+    most_correlated = most_correlated[:15]
+    
+    df = df.loc[:, most_correlated.index]
+    #df = df.drop(columns = 'higher_no')
+    
+    # Split into training/testing sets with 25% split
+    X_train, X_test, y_train, y_test = train_test_split(df, labels, 
+                                                        test_size = 0.25,
+                                                        random_state=42)
+    
+    return X_train, X_test, y_train, y_test
+
+#%%
+
+#X_train = X_train.dropna(axis=0).reset_index()
+
+df_enriched = df_enriched.dropna(axis=0)
+X_train, X_test, y_train, y_test = format_data(df_enriched)
+test = X_test.sample(n=20) # Note that some of the same filters as in the paper are correlated with PERMA...
+
+#%%
+
+print(X_train.shape)
+print(X_test.shape)
+
+#%%
+
+# Calculate mae and rmse
+def evaluate_predictions(predictions, true):
+    mae = np.mean(abs(predictions - true))
+    rmse = np.sqrt(np.mean((predictions - true) ** 2))
+    
+    return mae, rmse
+
+# Naive baseline is the median
+median_pred = X_train['PERMA'].median()
+median_preds = [median_pred for _ in range(len(X_test))]
+true = X_test['PERMA']
+
+# Display the naive baseline metrics
+mb_mae, mb_rmse = evaluate_predictions(median_preds, true)
+print('Median Baseline MAE: {:.4f}'.format(mb_mae))
+print('Median Baseline RMSE: {:.4f}'.format(mb_rmse))
+
+#%%
+
+# Standardize data before running model 
+
+
+#%%
+
+# Evaluate several ml models by training on training set and testing on testing set
+def evaluate(X_train, X_test, y_train, y_test):
+    # Names of models
+    #model_name_list = ['Linear Regression', 'ElasticNet Regression',
+                      #'Random Forest', 'Extra Trees', 'SVM',
+                       #'Gradient Boosted', 'Baseline']
+    
+    model_name_list = ['Linear Regression', 'ElasticNet Regression',
+                      'Random Forest', 'Extra Trees', 'Gradient Boosted',
+                      'Baseline']
+    
+    X_train = X_train.drop(columns='PERMA')
+    X_test = X_test.drop(columns='PERMA')
+    
+    # Instantiate the models
+    model1 = LinearRegression()
+    model2 = ElasticNet(alpha=1.0, l1_ratio=0.5)
+    model3 = RandomForestRegressor(n_estimators=50)
+    model4 = ExtraTreesRegressor(n_estimators=50)
+    #model5 = SVR(kernel='rbf', degree=3, C=1.0, gamma='auto')
+    model6 = GradientBoostingRegressor(n_estimators=20)
+    
+    # Dataframe for results
+    results = pd.DataFrame(columns=['mae', 'rmse'], index = model_name_list)
+    
+    # Train and predict with each model
+    #for i, model in enumerate([model1, model2, model3, model4, model5, model6]):
+    for i, model in enumerate([model1, model2, model3, model4, model6]):
+
+        model.fit(X_train, y_train)
+        predictions = model.predict(X_test)
+        
+        # Metrics
+        mae = np.mean(abs(predictions - y_test))
+        rmse = np.sqrt(np.mean((predictions - y_test) ** 2))
+        
+        # Insert results into the dataframe
+        model_name = model_name_list[i]
+        results.loc[model_name, :] = [mae, rmse]
+        print("...")
+    
+    # Median Value Baseline Metrics
+    baseline = np.median(y_train)
+    baseline_mae = np.mean(abs(baseline - y_test))
+    baseline_rmse = np.sqrt(np.mean((baseline - y_test) ** 2))
+    
+    results.loc['Baseline', :] = [baseline_mae, baseline_rmse]
+    
+    return results
+
+#%%
+
+results2 = evaluate(X_train, X_test, y_train, y_test)
 
 #%%
 
